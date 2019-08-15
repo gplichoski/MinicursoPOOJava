cb384a7cc8e634d4136ee446ce09527b8ed65d32
gplichoski <gplichoski@gmail.com>
2019-08-15 19:40:00 -0300

    Adiciona classe Turma

--- src/main/java/_5_desafio/Turma.java
+++ src/main/java/_5_desafio/Turma.java
@@ -5,10 +5,25 @@
  */
 package _5_desafio;
 
+import java.util.Calendar;
 /**
  *
  * @author g
  */
 public class Turma {
+    private Integer codigo;
+    private String sala;
+    private Calendar horario;
     
+    public boolean estaAberta() {
+        return true;
+    }
+    
+    public void defineProfessor(Professor professor) {
+        
+    }
+    
+    public void incluiAluno(Aluno aluno) {
+        
+    }
 }
