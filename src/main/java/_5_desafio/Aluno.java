73fc7bc6dec58de6dd1a482790d05042e937e91b
gplichoski <gplichoski@gmail.com>
2019-08-15 19:33:56 -0300

    Adiciona esqueleto do desafio

---
+++ src/main/java/_5_desafio/Aluno.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _5_desafio;
+
+/**
+ *
+ * @author g
+ */
+public class Aluno {
+    private String nome;
+    private Integer matricula;
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public Integer getMatricula() {
+        return matricula;
+    }
+    
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+}
---
+++ src/main/java/_5_desafio/Professor.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _5_desafio;
+
+/**
+ *
+ * @author g
+ */
+public class Professor {
+    private String nome;
+    private String titulacao;
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getTitulacao() {
+        return titulacao;
+    }
+    
+    public void setTitulacao(String titulacao) {
+        this.titulacao = titulacao;
+    }
+}
